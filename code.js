f0572c08573f3ae41e1a9fbea9fd7399c0291251
Jose Ezequiel <icap.ezequiel@gmail.com>
2022-08-23 22:00:20 -0500

    se crea personaje harley

--- code.js
+++ code.js
@@ -1,13 +1,16 @@
 let canvas = document.querySelector('canvas')
 let ctx = canvas.getContext('2d');
 
+let iniciar = false; //variable para controlar inicio
+
+// se crea objeto con todas las imagenes
 const imagen ={
     portada:'assets/imagenes/portada.png',
     instrucciones:'assets/imagenes/instrucciones.png',
     dinero: 'assets/imagenes/dinero.png',
     botom: 'assets/imagenes/botom.png',
     fondoPrincipal:'assets/imagenes/fondoPrincipal.png',
-    harley:'assets/imagenes/harley.png',
+    harley:'assets/imagenes/harleyPer.png',
     joker: 'assets/imagenes/joker.png',
     logo: 'assets/imagenes/logo.png',
     martillo:  'assets/imagenes/martillo.png',
@@ -19,7 +22,28 @@ const imagen ={
     espantapajaro: 'assets/imagenes/espantapajaro.png',        
 
 }
-//coloco valores de mi escnario
+class harley {
+    constructor(){
+        this.x = 25;
+        this.y = 365;
+        this.width = 90;
+        this.height = 90;
+        this.vida = 3;    //ver
+        this.puntuacion = 0; //ver
+        this.saltar = false  //veer
+        this.img= new Image()
+        this.img.src = imagen.harley;
+        this.img.onload=()=>{
+            this.dibujarHarley();
+        }        
+    }
+
+    dibujarHarley(){
+        ctx.drawImage(this.img,this.x,this.y,this.width,this.height)
+    }
+}          
+
+//coloco valores de mi escenario
 class background {
     constructor(){
         this.x = 0;
@@ -35,6 +59,7 @@ class background {
     //dibujo mi escenario
     dibujar(){
         ctx.drawImage(this.img,this.x,this.y,this.width,this.height);
+
     }
     //limpio mi escenario y coloco el siguiente
     cambiarImg(img){
@@ -43,40 +68,41 @@ class background {
         ctx.drawImage(this.img,this.x,this.y,this.width,this.height);
     }
 }
+//se inicializa fondo y personaje
 const fondo = new background();
+const personaje  = new harley() 
 
+//se crea click para borrar y mostrar instrucciones
 window.onload = ()=> {
     document.getElementById('boton-start').onclick=() =>{
         fondo.cambiarImg(imagen.instrucciones)
         const menu = document.getElementById('boton-start');
-        menu.style.display='none'        
-
+        menu.style.display='none' 
+        iniciar = true;     
     }                                                                                                                                                                                                                                                                                       
 }
 
-//se añade comandos (f continuar , espacio-disparar, arriba-saltar, etc)
+//se añade comando de keycode tecla f 
 //se manda a llamar siguiente escenario con case 70
 window.addEventListener('keydown',({keyCode}) => {
     switch(keyCode){
         case 70:
-            fondo.cambiarImg(imagen.fondoPrincipal)
-            break;
-        case 32:
-            harley.dispara()
-            break;
-        case 38:
-            harley.saltar()
-            break;
-        case 40:
-            harley.agacha()
-            break;
-        case 37:
-            harley.voltea()
-            break;
-        case 39:
-            harley.avanza()
-            break;
+           if(iniciar){
+             iniciarJuego()
+           }
+        break;
         
-    }
-    console.log(32)
+    }    
 })
+//se inicializa el juego
+const iniciarJuego =()=>{
+    iniciar= true;
+    fondo.cambiarImg(imagen.fondoPrincipal) 
+
+// se pinta mi personaje
+    setInterval(() => {
+        personaje.dibujarHarley()
+    },50)
+}
+
+
